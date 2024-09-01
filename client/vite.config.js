71b0f494f9012db59020dfd88f54b81267d6bd37
manascb1344 <zephop76593@gmail.com>
2024-09-01 21:57:31 +0530

    merge

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
-import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig(({ mode }) => {
