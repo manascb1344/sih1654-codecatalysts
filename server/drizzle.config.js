8fc170d2bd410c0b9bcce753290c974087fa512b
manascb1344 <zephop76593@gmail.com>
2024-09-01 18:36:48 +0530

    feat: add drizzle-kit to backend Introduces drizzle-kit for database migrations and schema management.

--- server/drizzle.config.js
+++ server/drizzle.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
 	dialect: "postgresql",
-	schema: "./src/db/schema.js",
+	schema: "./db/schema.js",
 	out: "./drizzle",
 	dbCredentials: {
 		url: "postgresql://sih1654_owner:4oGlfam9jXIW@ep-billowing-firefly-a1s7cnmm.ap-southeast-1.aws.neon.tech/sih1654?sslmode=require",
